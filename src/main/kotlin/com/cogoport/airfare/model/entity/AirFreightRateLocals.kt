851c10e9d21ee52a1e65bdbd7e7fdf5412feaff9
IamChetna <chetna.singh@cogoport.com>
2023-02-17 11:21:19 +0530

    locals get api

--- src/main/kotlin/com/cogoport/airfare/model/entity/AirFreightRateLocals.kt
+++ src/main/kotlin/com/cogoport/airfare/model/entity/AirFreightRateLocals.kt
@@ -1,5 +1,6 @@
 package com.cogoport.airfare.model.entity
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.micronaut.core.annotation.Introspected
 import io.micronaut.core.annotation.NonNull
 import io.micronaut.data.annotation.GeneratedValue
@@ -8,8 +9,10 @@ import io.micronaut.data.annotation.MappedEntity
 import java.util.*
 
 @Introspected
-@MappedEntity("air-freight-rate-locals")
+@MappedEntity("air_freight_rate_locals")
 open class AirFreightRateLocals(
     @field:Id @GeneratedValue @NonNull
-    val id: UUID
+    val id: UUID,
+    @JsonProperty("airline_id")
+    val airlineId: UUID?
 )
